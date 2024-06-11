f56d25ac753f33f643b34938ec103a3302cff0ce
Gustavo Orozco <gustavo.orozcom68@gmail.com>
2024-06-11 11:58:58 -0500

    Base project

---
+++ Invoicer/API/Controller/InvoicerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Request = Domain.DTOs.Request;
+//using Response = Domain.DTOs.Response;
+
+namespace API.Controller
+{
+    [Route("api/v1/[controller]/[action]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class InvoicerController : ControllerBase
+    {
+
+        /*[HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] ) { }*/
+    }
+}
---
+++ Invoicer/Domain/DTOs/Request/InvoiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.DTOs.Request
+{
+    public class InvoiceDto
+    {
+        public string Client { get; set; }
+        public DateOnly Date { get; set; }
+        public int Subtotal { get; set; }
+        public int Discount { get; set; }
+        public int Total { get; set; }
+    }
+}
